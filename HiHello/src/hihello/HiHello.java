5bffdde2199df80d8c98c523ba77db418b365e30
Alberto-Perilli-Peano-4A <56470905+Alberto-Perilli-Peano-4A@users.noreply.github.com>
2019-11-09 10:14:06 +0100

    Codice Java
    Aggiunto codice java

---
+++ HiHello/src/hihello/HiHello.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hihello;
+
+/**
+ *
+ * @author perilli.alberto
+ */
+public class HiHello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hi a = new Hi();
+        Hello b = new Hello();
+        a.start();
+        b.start();
+    }
+    
+}
